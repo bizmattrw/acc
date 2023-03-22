98150f6c5bd7e7e3640f35e51037b1568c593ca2
bizmattrw <biz.matt01@gmail.com>
2023-03-22 14:29:59 +0200

    latest

--- script.js
+++ script.js
@@ -10,35 +10,35 @@
  */
 
 function addRow(tableID) {
-	var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	if(rowCount < 10){							// limit the user from creating fields more than your limits
-		var row = table.insertRow(rowCount);
-		var colCount = table.rows[0].cells.length;
-		for(var i=0; i<colCount; i++) {
-			var newcell = row.insertCell(i);
-			newcell.innerHTML = table.rows[0].cells[i].innerHTML;
-		}
-	}else{
-		 alert("Maximum fields is 10.");
-			   
-	}
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    if (rowCount < 100) { // limit the user from creating fields more than your limits
+        var row = table.insertRow(rowCount);
+        var colCount = table.rows[0].cells.length;
+        for (var i = 0; i < colCount; i++) {
+            var newcell = row.insertCell(i);
+            newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+        }
+    } else {
+        alert("Maximum fields is 100.");
+
+    }
 }
 
 function deleteRow(tableID) {
-	var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	for(var i=0; i<rowCount; i++) {
-		var row = table.rows[i];
-		var chkbox = row.cells[0].childNodes[0];
-		if(null != chkbox && true == chkbox.checked) {
-			if(rowCount <= 1) { 						// limit the user from removing all the fields
-				
-				break;
-			}
-			table.deleteRow(i);
-			rowCount--;
-			i--;
-		}
-	}
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    for (var i = 0; i < rowCount; i++) {
+        var row = table.rows[i];
+        var chkbox = row.cells[0].childNodes[0];
+        if (null != chkbox && true == chkbox.checked) {
+            if (rowCount <= 1) { // limit the user from removing all the fields
+
+                break;
+            }
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+        }
+    }
 }
